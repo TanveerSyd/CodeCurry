3f6a5644ee731207b6ed80a7a984d079a2dc4308
Tanveer <reborn.illuminati@gmail.com>
2013-04-12 16:42:30 +1000

    DIV
    DIV

---
+++ MultiEndPointWCF/MultiEndPointWCF/IStudentService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace MultiEndPointWCF
+{
+    [ServiceContract]
+    public interface IStudentService
+    {
+        //[OperationContract]
+        //Reference.Repository.Student GetStudentById(int value);
+
+
+        [OperationContract]
+        //[WebGet(UriTemplate = "json/{id}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,           
+            UriTemplate = "json/")]
+        Student GetStudent();
+
+
+        // TODO: Add your service operations here
+    }
+}
---
+++ MultiEndPointWCF/MultiEndPointWCF/Service1.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using MultiEndPointWCF;
+using Reference.Repository;
+
+namespace MultiEndPointWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    [ServiceBehavior(AddressFilterMode = AddressFilterMode.Any)]
+    public class Service1 : IStudentService
+    {
+        //public Reference.Repository.Student GetStudentById(int value)
+        //{
+        //    StudentRepository rs = new StudentRepository();
+        //    return rs.GetStudent(17);
+
+        //    //return new Student { StudentID = 1, FirstName = "Rowan", LastName = "Atkinson", Age = 55 };
+        //}
+
+        public Student GetStudent()
+        {
+            StudentRepository rs = new StudentRepository();
+            //return rs.GetStudent(17).ToString();
+           // return "Bullocks";
+
+            return new Student { StudentID = 1, FirstName = "Rowan", LastName = "Atkinson", Age = 55 };
+        }
+
+    }
+}
